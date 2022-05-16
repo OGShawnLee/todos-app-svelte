19d574fe207ec39cd7dc8a61e4e33927a0b1b3e7
Shawn Lee <changoloco.evg@gmail.com>
2022-05-16 14:49:46 -0500

    :wheelchair: [feat](a11y): add aria-label to filter buttons

--- src/components/Filter.svelte
+++ src/components/Filter.svelte
@@ -1,3 +1,9 @@
+<script lang="ts" context="module">
+  function capitalise(str: string) {
+    return str[0].toUpperCase() + str.substring(1).toLowerCase();
+  }
+</script>
+
 <script lang="ts">
   import { todos } from '../state';
   import { useClassNameResolver } from 'malachite-ui/hooks';
@@ -9,7 +15,7 @@
   $: isSelected = $current === filter;
 
   const className = useClassNameResolver<'isSelected'>({
-    base: 'capitalize <sm:text-sm xl:text-base text-slate-600 font-bold transition duration-300 ease-out',
+    base: '<sm:text-sm xl:text-base text-slate-600 font-bold transition duration-300 ease-out',
     selected: {
       on: 'text-blue transform focus:text-cyan',
       off: 'hover:text-white focus:text-white',
@@ -17,6 +23,6 @@
   });
 </script>
 
-<button class={className({ isSelected })} on:click>
-  {filter.toLowerCase()}
+<button class={className({ isSelected })} on:click aria-label="Show {capitalise(filter)} Todos">
+  {capitalise(filter)}
 </button>
