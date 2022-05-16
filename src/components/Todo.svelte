459a31e377516e66af4072e1d9edefa2140966c8
Shawn Lee <changoloco.evg@gmail.com>
2022-05-16 14:25:50 -0500

    :sparkles: [feat]: prompt user before deleting todo

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import Switch from './Switch.svelte';
-  import { slide } from 'svelte/transition';
-  import { todos } from '../state';
+  import { Dialog, DialogDescription, DialogTitle } from 'malachite-ui/components';
+  import { fade, scale, slide } from 'svelte/transition';
+  import { inputElement, todos } from '../state';
   import { quadOut } from 'svelte/easing';
 
   export let isCompleted: boolean;
   export let value: string;
   export let id: number;
+
+  let open = false;
 </script>
 
 <article
@@ -20,11 +23,54 @@
   </h3>
   <button
     class="group ml-auto h-8 w-8 min-w-8 | bg-slate-800 text-slate-600 rounded-full transition duration-300 ease-out hover:bg-slate-700 focus:bg-slate-700"
-    on:click={() => todos.delete(id)}>
+    on:click={() => (open = true)}>
     <span class="sr-only"> Delete Todo </span>
     <i class="bx bx-x text-2xl group-hover:text-cyan group-focus:text-cyan" />
   </button>
 </article>
+
+<Dialog
+  class="fixed inset-0 | flex items-center justify-center"
+  bind:open
+  let:close
+  let:overlay
+  let:content>
+  <div
+    class="fixed inset-0 | bg-slate-900/70 backdrop-filter backdrop-blur-[1px]"
+    use:overlay
+    transition:fade />
+
+  <div
+    class="max-w-xs p-8 z-10 | bg-slate-800 rounded-md | sm:max-w-lg"
+    use:content
+    transition:scale={{ duration: 300, start: 0.75, easing: quadOut }}>
+    <DialogTitle class="text-white font-bold sm:text-lg">
+      Delete "{value}"?
+    </DialogTitle>
+    <DialogDescription class="text-sm">
+      Do you want to delete this <b class="text-slate-100">
+        {isCompleted ? 'completed' : 'incompleted'}
+      </b>
+      todo? This action will be
+      <strong class="text-slate-100"> irreversible </strong>.
+    </DialogDescription>
+    <div class="mt-6 | flex items-center gap-6">
+      <button
+        class="px-6 py-2 | border-2 border-transparent rounded-md transition-colors duration-300 ease-out focus:(text-cyan border-cyan) hover:text-pink"
+        on:click={close}>
+        Cancel
+      </button>
+      <button
+        class="px-6 py-2 | border-2 border-transparent rounded-md transition-colors duration-300 ease-out focus:(text-pink border-pink) hover:text-pink"
+        on:click={() => {
+          close($inputElement);
+          todos.delete(id);
+        }}>
+        Delete
+      </button>
+    </div>
+  </div>
+</Dialog>
 
 <style>
   .completed {
