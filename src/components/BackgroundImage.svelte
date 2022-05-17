8b385ceca7a5b46aad1cf8e3af71525efdce7e11
Shawn Lee <changoloco.evg@gmail.com>
2022-05-17 18:08:42 -0500

    :mag: [fix](seo): remove wrong image attributes

--- src/components/BackgroundImage.svelte
+++ src/components/BackgroundImage.svelte
@@ -13,13 +13,8 @@
   <picture
     class="fixed top-0 left-0 right-0 -z-10 h-[200px] | block | sm:h-[300px]"
     aria-hidden="true">
-    <source media="(max-width: 375px)" srcset={bgMobile} sizes="375x200" />
-    <source media="(min-width: 375px)" srcset={bgDesktop} sizes="1400x300" />
-    <img
-      class="h-full w-full object-center object-cover"
-      src={bgMobile}
-      sizes="375x200"
-      alt=""
-      transition:fade />
+    <source media="(max-width: 375px)" srcset={bgMobile} />
+    <source media="(min-width: 375px)" srcset={bgDesktop} />
+    <img class="h-full w-full object-center object-cover" src={bgMobile} alt="" transition:fade />
   </picture>
 {/key}
